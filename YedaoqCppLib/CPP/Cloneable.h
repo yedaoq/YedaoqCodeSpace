bedf3d8f97ec42625a247f63d616ebcf1bdc662c
yedaoq <yedaoq@gmail.com>
2012-03-15 23:52:26 +0800

    Normal

--- YedaoqCppLib/CPP/Cloneable.h
+++ YedaoqCppLib/CPP/Cloneable.h
@@ -8,6 +8,7 @@
 @     
 /* ___________________________________________________________________________*/
 #pragma once
+#include "\boost\assert.hpp"
 
 #ifndef interface
 #define interface struct
@@ -30,10 +31,93 @@
 interface ICloneable DERIVEFROMIREFOBJECT
 {
 	virtual ICloneable* Clone() const = 0;
+	virtual void		Delete() = 0;
 };
 
 template<typename T, typename I>
 struct CCloneable : public I
 {
 	virtual ICloneable* Clone() const { return new T(static_cast<const T&>(*this)); }
+	virtual void		Delete() { delete static_cast<T*>this; }
+};
+
+template<typename T>
+struct cloned_ptr
+{
+private:
+	T * px;
+
+	void operator==( cloned_ptr const& ) const;
+	void operator!=( cloned_ptr const& ) const;
+
+	inline T* clone(T const& obj) const { return static_cast<T*>(obj.Clone()); }
+
+public:
+
+	typedef T element_type;
+	typedef cloned_ptr<T> this_type;
+
+	explicit cloned_ptr( T * p = 0 ): px( p ) {} // never throws
+	explicit cloned_ptr(const T& obj) : px(clone(obj))	{}
+
+	cloned_ptr(cloned_ptr const & other) : px(clone(*other)) {}
+
+	~cloned_ptr() // never throws
+	{
+		if(px)
+		{
+			T* tmp = px;
+			px = 0;
+			tmp->Delete();
+		}
+	}
+
+	cloned_ptr& operator=(cloned_ptr const & other)
+	{
+		BOOST_ASSERT( &other != this ); // catch self-assign errors
+		reset(clone(*other));
+		return *this;
+	}
+
+	cloned_ptr& operator=(T const& obj)
+	{
+		reset(clone(obj));
+		return *this;
+	}
+
+	cloned_ptr& operator=(T* p)
+	{
+		reset(p);
+		return *this;
+	}
+
+	void reset(T * p = 0) // never throws
+	{
+		BOOST_ASSERT( p == 0 || p != px ); // catch self-reset errors
+		this_type(p).swap(*this);
+	}
+
+	T & operator*() const // never throws
+	{
+		BOOST_ASSERT( px != 0 );
+		return *px;
+	}
+
+	T * operator->() const // never throws
+	{
+		BOOST_ASSERT( px != 0 );
+		return px;
+	}
+
+	T * get() const // never throws
+	{
+		return px;
+	}
+
+	void swap(cloned_ptr & b) // never throws
+	{
+		T * tmp = b.px;
+		b.px = px;
+		px = tmp;
+	}
 };
